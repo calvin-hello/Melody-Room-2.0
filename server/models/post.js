2fd7a721e7e95fbe753c7d41c16a9ad3d0a7072a
Calvin Mwin-Era <45calvin45@gmail.com>
2026-05-07 14:22:57 -0700

    feat: schema

--- server/models/post.js
+++ server/models/post.js
@@ -1,7 +1,64 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema()
+const postSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: "authorType",
+    required: true,
+  },
 
-const Post = mongoose.model("Post", postSchema)
+  authorType: {
+    type: String,
+    enum: ["User", "Artist"],
+    required: true,
+  },
 
-export default Post
+  content: {
+    type: String,
+    required: true,
+    maxlength: 1000,
+  },
+
+  image: {
+    type: String,
+    default: "",
+  },
+
+  song: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Song",
+  },
+
+  album: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Album",
+  },
+
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }],
+
+  comments: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    text: String,
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }],
+
+  tags: [{
+    type: String,
+  }],
+
+}, {
+  timestamps: true,
+});
+
+export default mongoose.model("Post", postSchema);
--- server/models/song.js
+++ server/models/song.js
@@ -1,6 +1,60 @@
 import mongoose from "mongoose"
 
-const songSchema = new mongoose.Schema()
+const songSchema = new mongoose.Schema({
+     title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  artist: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Artist",
+    required: true,
+  },
+
+  duration: {
+    type: Number, // seconds
+    required: true,
+  },
+
+  audioUrl: {
+    type: String,
+    required: true,
+  },
+
+  coverImage: {
+    type: String,
+    default: "",
+  },
+
+  genre: {
+    type: String,
+    default: "Unknown",
+  },
+
+  plays: {
+    type: Number,
+    default: 0,
+  },
+
+  likes: {
+    type: Number,
+    default: 0,
+  },
+
+  releaseDate: {
+    type: Date,
+  },
+
+  explicit: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true,
+
+})
 
 const Song = mongoose.model("Song", songSchema)
 
