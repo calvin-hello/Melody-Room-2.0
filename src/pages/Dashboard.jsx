9ef84c889f4ad2ae99a526b72d37f192784cf85c
Long Nguyen <qnguyen78@my.bcit.ca>
2026-05-08 06:27:28 -0700

    route for proflie/username

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -21,7 +21,7 @@ export default function Dashboard() {
             For You
           </Link>
           {/* <Link>Following</Link> */}
-          <Link to="/profile">Profile</Link>
+          <Link to={`/profile/${username}`}>Profile</Link>
           <button onClick={handleLogout}>Logout</button>
         </nav>
       </header>
